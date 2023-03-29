c83c8c7dd3f03f029c152db56d8ff00abfc99bcf
RizwanAhmed3000 <rizz112khan@gmail.com>
2023-03-29 08:47:29 +0500

    array map(), filter(), find(), forEach()

--- app2.js
+++ app2.js
@@ -1,26 +1,36 @@
-function myFunction(abc){
-    console.log('hi')
-    abc()
-}
+// function myFunction(abc){
+//     console.log('hi')
+//     abc()
+// }
 
-function myFunction2(){
-    console.log('bye')
-}
-
-
+// function myFunction2(){
+//     console.log('bye')
+// }
 
 
-myFunction(myFunction2);
+
+
+// myFunction(myFunction2);
   
 
 
 
-var numbers = [1,2,3,4,5,6,7,8,9,10];
-var newNumbers = numbers.map((num)=>{
-    return num*5;
-})
-var names = ["ali","bilal","fahad"];
-var fulNames = names.map((item)=>{
-    return `${item} Ahmed`;
-})
+var numbers = [1,2,3,4,5,6,7,8,9,10,12,34,10,6];
+var newNumbers = numbers.filter((val)=> val >= 3 && val < 12).map((num)=> num*5)
+console.log(newNumbers)
+
+
+
+var names = ["ali","bilal","fahad", 'rizwan'];
+var fulNames = names.map((item)=>`${item.toString().slice(0,1).toUpperCase()}${item.toString().slice(1).toLowerCase()} Ahmed`)
 console.log(fulNames);
+
+var selectedNum = numbers.filter((val)=>{
+    return val >= 2 && val < 34;
+})
+console.log(selectedNum);
+
+var findNum = numbers.find((val)=> val > 5);
+console.log(findNum)
+
+var forEachVal = numbers.forEach((val, indx)=> console.log((val * 3) + " ==>> " + indx))
